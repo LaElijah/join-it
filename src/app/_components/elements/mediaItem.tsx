fddb14f6b2700892b5f034bf2e38a8e5a23912c4
Lloyd Elijah Allotey <elijah.allotey.w@gmail.com>
2023-10-07 03:10:30 +0000

    chore: pushing unfinished

--- src/app/_components/elements/mediaItem.tsx
+++ src/app/_components/elements/mediaItem.tsx
@@ -8,31 +8,59 @@ import Image from "next/image"
 
 
 export default function MediaItem(props: Media) {
-    
+
 
     if (props.type === "book") {
         const item = props.data as BookData
         return (
             <div className={styles.container}>
-                {item?.isbn && <p>ISBN: {item.isbn}</p>}
-                {item?.cover && <Image src={item.cover} alt="cover"/>}
-                {item?.publisher && <p>Publisher: {item.publisher}</p>}
-                {item?.datePublished && <p>Date Published: {item.datePublished}</p>}
-                {item?.language && <p>Language: {item.language}</p>}
-                {item?.description && <p>Description: {item.description}</p>}
-                {item?.authors && <p>Authors: {item.authors.join(", ")}</p>}
-                {item?.keywords && <p>Keywords: {item.keywords.join(", ")}</p>}
+                {
+                    item?.isbn
+                    && <p>ISBN: {item.isbn}</p>
+                }
+                {
+                    item?.cover
+                    && <img
+                        src={item.cover}
+                        alt="cover" width={64}
+                        height={64}
+                    />
+                }
+                {
+                    item?.publisher
+                    && <p>Publisher: {item.publisher}</p>
+                }
+                {
+                    item?.datePublished
+                    && <p>Date Published: {item.datePublished}</p>
+                }
+                {
+                    item?.language
+                    && <p>Language: {item.language}</p>
+                }
+                {
+                    item?.description
+                    && <p>Description: {item.description}</p>
+                }
+                {
+                    item?.authors
+                    && <p>Authors: {item.authors.join(", ")}</p>
+                }
+                {
+                    item?.keywords
+                    && <p>Keywords: {item.keywords.join(", ")}</p>
+                }
             </div>
         )
     }
     else if (props.type === "video") {
         let item = props.data as VideoData
 
-    return (
-        <div className={styles.container}>
-            <p>Video</p>
-            {item && <p>{item.description}</p>}
-        </div>
-    )
-}
+        return (
+            <div className={styles.container}>
+                <p>Video</p>
+                {item && <p>{item.description}</p>}
+            </div>
+        )
+    }
 }
--- src/app/_components/featuredMedia.tsx
+++ src/app/_components/featuredMedia.tsx
@@ -8,16 +8,30 @@ import Image from "next/image"
 
 
 const MediaContent = ({type, data}: any) => {
+    
+}
+
+export default function FeaturedMedia(props: { mediaData: Media }) {
+
+    const { title, type, subjects, metadata, reactions, data } = props.mediaData
+
+
     if (type === "book") {
         return (
-            <div className={styles.media}>
-                <img src={(data as BookData).cover} alt="cover"  />
+            <div className={styles.container}>
+                <h1>Featured Media</h1>
+                <img 
+                height={200}
+                width={200}
+                src={(data as BookData).cover} 
+                alt="Book Cover"  
+                />
             </div>
         )
     }
     else if(type === "video") {
         return (
-            <div className={styles.media}>
+            <div className={styles.container}>
                 <iframe
                     width="707"
                     height="480"
@@ -28,41 +42,6 @@ const MediaContent = ({type, data}: any) => {
             </div>
         )
     }
-}
 
-export default function FeaturedMedia(props: { mediaData: Media }) {
-
-    const { title, type, subjects, metadata, reactions, data } = props.mediaData
-
-
-
-    return (
-        <div className={styles.container}>
-
-            <div className={styles.media}>
-                <MediaContent 
-                type={type}
-                data={data}
-                />
-                
-            </div>
-
-            <div className={styles.info}>
-                <h3>{title}</h3>
-                <p>{type}</p>
-                <p>{subjects.join(", ")}</p>
-                <p>{metadata.submittedBy}</p>
-                <p>{metadata.verified ? "Verified" : "Not Verified"}</p>
-                <p>{metadata.dateSubmitted}</p>
-                <p>{reactions.length}</p>
-                <p>{data.description}</p>
-
-            </div>
-
-
-
-
-
-        </div>
-    )
+    
 }
